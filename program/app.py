90d9524e3a7d72a332b92c42209b6037efa0c2ac
Edvin Boul <kittenparry@users.noreply.github.com>
2018-12-18 13:08:09 +0300

    Adding/removing tags
    Read tags from db\tags.json {filename: tags}
    Return read tags to the main page
    Ability to add/remove tags, dumps the updated db to the .json file
    Invisible form elements to convey the number of images and path information
    Select all function to images (it's actually reverse the selection, needs fixing)

--- program/app.py
+++ program/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 from program.tree import *
 from program.files import get_files
 
@@ -6,7 +7,15 @@ app = Flask(__name__)
 
 title = "File Tagger"
 path = r"E:\from4chan\4chan"
-test_tags = ["screenshot", "meme", "high_resolution", "background", "food", "test_tags"]
+path_db = r"db\tags.json"
+#check if it crashes if the files doesn't exist
+with open(path_db) as di:
+    tag_dic = json.load(di)
+tags = []
+for v in tag_dic.values():
+    tags += v
+tags = list(set(tags))
+#probably rerun the function above when tags are edited
 @app.context_processor
 def processor():
     def split(e):
@@ -22,7 +31,7 @@ def processor():
 tree_maker("└📁", path)
 @app.route('/')
 def main():
-    return render_template('main.html', title=title, ftab=ftab, idp=idp, tags=test_tags)
+    return render_template('main.html', title=title, ftab=ftab, idp=idp, tags=tags)
 
 @app.route('/files/', defaults={'val': 'start'})
 @app.route('/files/<string:val>')
@@ -33,6 +42,57 @@ def files(val):
         files, thumbs = get_files(idp[val])
         thumbs = ['thumbs/' + thumb for thumb in thumbs]
         return render_template('files.html', val=val, idp=idp, files=files, thumbs=thumbs)
+
+@app.route('/handle_files', methods=['POST'])
+def handle_files():
+    im = []
+    path = []
+    #tag_dic = {}
+    with open(path_db) as di: #get previous tags
+        tag_dic = json.load(di)
+    t = request.form['text_tags'].split(' ') #gets the entered value
+    n = int(request.form['lab_len']) #get the number of images
+    for i in range(n):
+        try: #only the selected ones
+            im.append(request.form[f'images{i}'])
+            path.append(request.form[f'path{i}'])
+        except:
+            pass
+    try:
+        a = request.form['sub_add']
+    except:
+        a = None
+    try:
+        r = request.form['sub_rem']
+    except:
+        r = None
+    #add path to the dict if they don't exist
+    for p in path:
+        if p not in tag_dic:
+            tag_dic.update({p:[]})
+    #if add button clicked
+    if r is None:
+        msg = f"Added tags {t} to the images {im}."
+        for i in range(len(path)):
+            tag_dic[path[i]] = list(set(tag_dic[path[i]] + t))
+            #add tags to the path
+            #remove duplicates (set())
+    else:
+        msg = f"Removed tags {t} from the images {im}."
+        for i in range(len(path)):
+            try:
+                for tag in t:
+                    if tag in tag_dic[path[i]]:
+                        tag_dic[path[i]].remove(tag)
+            except:
+                pass
+    for p in path:
+        msg += f"{p}\n"
+
+    with open(path_db, 'w') as f:
+        json.dump(tag_dic, f)
+
+    return msg
 
 
 if __name__ == '__main__':
--- program/static/js/script.js
+++ program/static/js/script.js
@@ -1,8 +1,12 @@
 function get_files(tab){
     bed = document.getElementById("iframe_files").src = "/files/" + tab;
 }
-function check_this(c){
-    cbox = c.parentElement.firstElementChild;
+function check_this(c, i){
+    if(i == 0){
+        cbox = c.parentElement.firstElementChild;
+    }else{
+        cbox = c;
+    }
     if(cbox.checked){
         cbox.checked = false;
         c.parentNode.style.backgroundColor = "#fff";
@@ -10,4 +14,11 @@ function check_this(c){
         cbox.checked = true;
         c.parentNode.style.backgroundColor = "#337ab7";
     }
+}
+function sel_all(n) {
+    document.innerHTML += n;
+    for(i=0;i<n;i++){
+        //document.getElementById("check" + i).checked = true;
+        check_this(document.getElementById(("check" + i)), 1)
+    } //this is flawed because it reverses the selection, not selects all
 }
